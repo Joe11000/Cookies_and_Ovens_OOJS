b97e16715b809c670f5d9e58bba5e08a7d2f25c0
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-03 10:02:45 -0500

    added Cookie class

---
+++ main.js
@@ -0,0 +1,36 @@
+function Cookie(name, total_bake_time){
+  this.name = name;
+  this.total_bake_time;
+
+  var current_bake_time = 0;
+
+  this.bake = function(){
+    current_bake_time++;
+  }
+
+  this.getStatus = function()
+  {
+
+    if(current_bake_time === 0)
+      return "raw";
+    else if(current_bake_time < total_bake_time)
+      return "still_gooey";
+    else if(current_bake_time === total_bake_time)
+      return "just_right";
+    else if(current_bake_time <= total_bake_time + 3)
+      return "crispy †";
+    else if(current_bake_time <= total_bake_time + 5)
+      return "you done fucked up";
+    else
+      return "kitchen fire :o";
+  }
+
+  this.info = function(){
+    alert("I am a " + name + " with a bake time of " + total_bake_time + ". current bake time is " + current_bake_time)
+  }
+}
+
+// $(document).ready(function() {
+//   $('new_batch').on('')
+// });
+ 
